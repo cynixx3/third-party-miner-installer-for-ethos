a057371f3a6ff86678f0f9ef13780661ac2a393f
unsivil <unsivilaudio@gmail.com>
2019-02-18 12:35:05 -0700

    Initial commit

--- opt/ethos/lib/minerprocess.php
+++ opt/ethos/lib/minerprocess.php
@@ -1085,7 +1085,7 @@ function start_miner()
 	 * XMRIG-AMD
 	 ********************************/
 	
-	if ($miner == "xmrig-amd") {
+	if ($miner == "xcash-amd") {
 	    delete_old_api_port();
 	    $apiport = select_api_port();
 	    // shell_exec("cp /opt/ethos/etc/xmrig-amd-config.json /var/run/ethos/xmrig-config.json");
@@ -1097,11 +1097,11 @@ function start_miner()
 	    $extraflags .= "--api-port $apiport --print-time 15";
 	    if(!preg_match("/--donate-level/", $flags)) { $flags .= " --donate-level 5"; }
 	    if($stratumtype == "nicehash") { $flags .= " --nicehash"; }
-	    if(!preg_match("/-a/", $flags)) { $flags .= " -a cryptonight"; }
-	    if(preg_match("/--variant (\S*).*/", $flags, $matches)) {
-	        $variant = $matches[1]; } else {
-	            $variant = " -1";
-	        }
+// 	    if(!preg_match("/-a/", $flags)) { $flags .= " -a cryptonight"; }
+// 	    if(preg_match("/--variant (\S*).*/", $flags, $matches)) {
+// 	        $variant = $matches[1]; } else {
+// 	            $variant = " -1";
+// 	        }
 	        
 	        // always show worker here
 	        $worker = trim(`/opt/ethos/sbin/ethos-readconf worker`);
@@ -1142,7 +1142,7 @@ function start_miner()
 	$miner_path['teamredminer'] = "/usr/bin/screen -c /opt/ethos/etc/screenrc.teamredminer -l -L -dmS teamredminer /opt/miners/teamredminer/teamredminer";
 	$miner_path['ewbf-equihash'] = "/usr/bin/screen -c /opt/ethos/etc/screenrc.ewbf-equihash -l -L -dmS ewbf-equihash /opt/miners/ewbf-equihash/ewbf-equihash";
 	$miner_path['lolminer'] = "/usr/bin/screen -c /opt/ethos/etc/screenrc.lolminer -l -L -dmS lolminer /opt/miners/lolminer/lolMiner";
-	$miner_path['xmrig-amd'] = "/usr/bin/screen -c /opt/ethos/etc/screenrc.xmrig-amd -l -L -dmS xmrig-amd /opt/miners/xmrig-amd/xmrig-amd";
+	$miner_path['xcash-amd'] = "/usr/bin/screen -c /opt/ethos/etc/screenrc.xcash-amd -l -L -dmS xcash-amd /opt/miners/xcash-amd/xcash-amd";
 	
 			
 	$start_miners = select_gpus();
@@ -1176,7 +1176,7 @@ function start_miner()
 		$miner_params['teamredminer'] = $flags ." ". $pools;
 		$miner_params['ewbf-equihash'] = "--config /var/run/ethos/ewbf-equihash.conf";
 		$miner_params['lolminer'] = $flags;
-		$miner_params['xmrig-amd'] = $flags ." ". $pools;
+		$miner_params['xcash-amd'] = $flags ." ". $pools;
 		
 		$miner_suffix['avermore'] = " " . $mine_with . " " . $extraflags;
 		$miner_suffix['dstm-zcash'] = " " . $mine_with . " " . $extraflags;
@@ -1200,7 +1200,7 @@ function start_miner()
 		$miner_suffix['xtl-stak'] = " " . $extraflags;
 		$miner_suffix['teamredminer'] = " " . $mine_with . " " . $extraflags;
 		$miner_suffix['lolminer'] = "";
-		$miner_suffix['xmrig-amd'] = " ". $mine_with ." ". $extraflags;
+		$miner_suffix['xcash-amd'] = " ". $mine_with ." ". $extraflags;
 		
 		$command = "su - ethos -c \"" . escapeshellcmd($miner_path[$miner] . " " . $miner_params[$miner]) . " $miner_suffix[$miner]\"";
 		$command = str_replace('\#',"#",$command);
